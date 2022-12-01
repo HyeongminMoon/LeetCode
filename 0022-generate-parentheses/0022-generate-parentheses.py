4c5c2e26bb9d2eff97cc8388d04d25f3fe8f6b01
Hyeongmin Moon <mohomin123@gmail.com>
2022-12-01 15:50:18 +0900

    Time: 26 ms (99.67%), Space: 14.3 MB (40.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,17 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        dp = [[]] #dp[0] = []
+        dp = [[""]] #dp[0] = [""]
         dp.append(set(["()"])) #dp[1] = ["()"]
         
         for i in range(2, n+1):
             temp = set()
-            for prefix in dp[i-1]:
-                temp.add("(" + prefix + ")")
-
-            for k in range(1, i):
-                for sub1 in dp[i-k]:
+            for k in range(0, i):
+                for sub1 in dp[i-k-1]:
                     for sub2 in dp[k]:
-                        temp.add(sub1+sub2)
+                        temp.add('('+sub1+')'+sub2)
                 
             dp.append(temp)
             
