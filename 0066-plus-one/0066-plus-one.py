ecefffe7ce273691321788569a7c3063e85c842f
Hyeongmin Moon <mohomin123@gmail.com>
2023-04-02 20:04:52 +0900

    Time: 35 ms (53.80%), Space: 14 MB (5.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -5,13 +5,9 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        for idx in range(len(digits) - 1, -1, -1):
-            if digits[idx] != 9:
-                digits[idx] += 1
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
                 return digits
-            # 올림
-            digits[idx] = 0
-        
-        # 여기까지 왔다면, 맨 앞자리가 추가로 필요
-        return [1] + digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
