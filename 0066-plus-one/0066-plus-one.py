7584e5e8874291129288bf511764f967acb74fe0
Hyeongmin Moon <mohomin123@gmail.com>
2023-04-02 20:02:16 +0900

    Time: 41 ms (13.33%), Space: 13.9 MB (5.93%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,3 +1,17 @@
+# 한자리씩 계산
+# time: O(n)
+
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        return [int(s) for s in str(int(''.join([str(d) for d in digits]))+1)]
+        
+        for idx in range(len(digits) - 1, -1, -1):
+            if digits[idx] != 9:
+                digits[idx] += 1
+                return digits
+            # 올림
+            digits[idx] = 0
+        
+        # 여기까지 왔다면, 맨 앞자리가 추가로 필요
+        return [1] + digits
